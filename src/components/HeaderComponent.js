fe7f7d23aef7752f3f8dab7496568a8f28dd5626
Chris Dickson <chrisrydickson@gmail.com>
2022-06-05 17:55:43 -0500

    Print display none

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,23 +1,23 @@
-import { useState } from "react"
-import { Nav, Navbar, NavbarToggler, Collapse, NavItem } from "reactstrap"
-import { NavLink } from "react-router-dom"
+import { useState } from "react";
+import { Nav, Navbar, NavbarToggler, Collapse, NavItem } from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    const [menuOpen, setMenuOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
         <>
-            <Navbar dark sticky='top' expand='md'>
+            <Navbar dark sticky="top" expand="md">
                 <NavbarToggler onClick={() => setMenuOpen(!menuOpen)} />
-                <Collapse isOpen={menuOpen} navbar className='justify-content-around'>
-                    <Nav navbar className='justify-content-center'>
+                <Collapse isOpen={menuOpen} navbar className="justify-content-around">
+                    <Nav navbar className="justify-content-center">
                         <NavItem>
-                            <NavLink className='nav-link' to='/'>
+                            <NavLink className="nav-link" to="/">
                                 Home
                             </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className='nav-link' to='/about'>
+                            <NavLink className="nav-link" to="/about">
                                 About
                             </NavLink>
                         </NavItem>
@@ -25,7 +25,7 @@ const Header = () => {
                 </Collapse>
             </Navbar>
         </>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
