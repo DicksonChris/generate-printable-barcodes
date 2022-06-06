1257134d8be1992ea3b82600849f83066f0012b9
Chris Dickson <chrisrydickson@gmail.com>
2022-06-05 19:02:50 -0500

    Change print settings

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,44 +1,46 @@
-import BarcodeLabel from "./BarcodeLabel"
-import { forwardRef } from "react"
+import BarcodeLabel from "./BarcodeLabel";
+import { forwardRef } from "react";
 
-const Page = forwardRef(({ itemName, barcodeDims, pageCols, pageRows, pageDims: {pageWidth, pageHeight} }, ref) => {
-    let gridTemplateColumns = '';
-    for (let idx = 0; idx < pageCols; idx++) {
-        gridTemplateColumns += '1fr '
-    }
-    const style = {
-        width: "8.5in",
-        height: '11in',
-        maxHeight: "11in",
-        display: "grid",
-        overflow: "hidden",
-        gridTemplateColumns
-    }
-
-    const PageContainer = () => {
-        const barcodes = []
-        const cellStyle = {
-            display: "inline-block"
+const Page = forwardRef(
+    ({ itemName, barcodeDims, pageCols, pageRows, pageDims: { pageWidth, pageHeight } }, ref) => {
+        let gridTemplateColumns = "";
+        for (let idx = 0; idx < pageCols; idx++) {
+            gridTemplateColumns += "1fr ";
         }
-        const pageItems = pageRows * pageCols
-        for (let idx = 0; idx < pageItems; idx++) {
-            barcodes.push(
-                <BarcodeLabel
-                    style={cellStyle}
-                    itemName={itemName}
-                    key={`cell-${idx}`}
-                    barcodeDims={barcodeDims}
-                />
-            )
-        }
-        return <div style={style}>{barcodes}</div>
+        const style = {
+            width: "8.5in",
+            height: "11in",
+            maxHeight: "11in",
+            display: "grid",
+            overflow: "hidden",
+            gridTemplateColumns,
+        };
+
+        const PageContainer = () => {
+            const barcodes = [];
+            const cellStyle = {
+                display: "inline-block",
+            };
+            const pageItems = pageRows * pageCols;
+            for (let idx = 0; idx < pageItems; idx++) {
+                barcodes.push(
+                    <BarcodeLabel
+                        style={cellStyle}
+                        itemName={itemName}
+                        key={`cell-${idx}`}
+                        barcodeDims={barcodeDims}
+                    />
+                );
+            }
+            return <div style={style}>{barcodes}</div>;
+        };
+
+        return (
+            <div ref={ref} style={{ transform: `scaleX(${pageWidth}) scaleY(${pageHeight})` }}>
+                <PageContainer />
+            </div>
+        );
     }
+);
 
-    return (
-        <div ref={ref} className='d-print-block' style={{ transform: `scaleX(${pageWidth}) scaleY(${pageHeight})` }}>
-            <PageContainer />
-        </div>
-    )
-})
-
-export default Page
+export default Page;
