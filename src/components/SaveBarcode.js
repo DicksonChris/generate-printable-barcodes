c3b5bf266294a8923253cea9df3e25f6f07392d1
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2022-06-07 02:22:21 -0500

    Sanity input validation for barcode

--- src/components/SaveBarcode.js
+++ src/components/SaveBarcode.js
@@ -1,15 +1,22 @@
 import { saveSvgAsPng } from "save-svg-as-png"
 import { RiSave3Fill } from "react-icons/ri"
 import { useBarcode } from "./BarcodeContext"
+import timeout from "../utils/timeout"
 
-const SaveBarcode = ({ itemName }) => {
+const SaveBarcode = ({ itemName, setShowAlert, setAlertText }) => {
     const contextElement = useBarcode()
 
     return (
         <div>
             <button
+                id='save-barcode-button'
                 className='btn link-danger p-0 m-0'
-                onClick={() => {
+                onClick={(event) => {
+                    event.preventDefault()
+                    if (timeout(itemName, setShowAlert, setAlertText)) { 
+                        return
+                    } 
+
                     const svg = contextElement.querySelector("svg")
                     const svgOuterHeight = parseInt(svg.getAttribute("height"), 10) + 2
                     const svgOuterWidth = parseInt(svg.getAttribute("width"), 10) + 2
@@ -21,6 +28,9 @@ const SaveBarcode = ({ itemName }) => {
                         height: svgOuterHeight,
                         width: svgOuterWidth
                     })
+                }}
+                onMouseDown={(event)=>{
+                    event.preventDefault()
                 }}
             >
                 <span title="Download barcode as image">
--- src/utils/timeout.js
+++ src/utils/timeout.js
@@ -1,12 +1,42 @@
-const timeout = (text, callback) => {
+const timeout = (text, setShowAlert, setAlertText) => {
+    // Check if the text is empty
     if (text.length === 0) {
-      callback(true)
+      setShowAlert(true)
+        setAlertText("Please enter a barcode")
       setTimeout(() => {
-        callback(false)
+        setShowAlert(false)
       }, 5000)
       return true
     }
+
+    // Check if the text starts with a space
+    if (text.charAt(0) === " ") {
+        setShowAlert(true)
+          setAlertText("Barcode should not START with a space")
+        setTimeout(() => {
+          setShowAlert(false)
+        }, 5000)
+        return true
+      } 
+
+  // Check if the text ends with a space
+    const endsWithSpace = (text) => {
+        if (text.charAt(text.length - 1) === " ") {
+            return true
+        }
+        return false
+    }
+
+    if (endsWithSpace(text)) {
+        setShowAlert(true)
+            setAlertText(`Barcode should not END with a space`)
+        setTimeout(() => {
+            setShowAlert(false)
+        }, 5000)
+        return true
+    }
     return false
-  }
+}
+
   
   export default timeout
